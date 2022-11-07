b01856db72dc062cc6d4c08a1e9008cf92146734
caioorleans <caioorleansdm@hotmail.com>
2022-11-07 13:27:37 -0300

    Criado o input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-list';
+
+  allItems = [
+    {
+      descricao:"Comer salada",
+      feito:true
+    },
+    {
+      descricao:"Ir para a academia",
+      feito:false
+    },
+    {
+      descricao:"Estudar angular",
+      feito:true
+    },
+    {
+      descricao:"Dormir cedo",
+      feito:false
+    }
+  ];
+
+  addItem(descricao:string){
+    this.allItems.push({
+      descricao,
+      feito:false
+    })
+  }
 }
