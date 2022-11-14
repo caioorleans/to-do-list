ea64994121fd48df0639126e5b029233e5912abc
caioorleans <caioorleansdm@hotmail.com>
2022-11-14 13:53:54 -0300

    Itens add e del salvos no LocalStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,27 @@
 import { Component } from '@angular/core';
 import { Item } from 'src/models/item';
+import { StorageService } from './services/storage.service';
+
+const todoListStorageKey = 'Todo_List';
+
+const defaultToDoList = [
+  {
+    descricao:"Comer salada",
+    feito:true
+  },
+  {
+    descricao:"Ir para a academia",
+    feito:false
+  },
+  {
+    descricao:"Estudar angular",
+    feito:true
+  },
+  {
+    descricao:"Dormir cedo",
+    feito:false
+  }
+];
 
 @Component({
   selector: 'app-root',
@@ -9,33 +31,26 @@ import { Item } from 'src/models/item';
 export class AppComponent {
   title = 'to-do-list';
 
-  allItems = [
-    {
-      descricao:"Comer salada",
-      feito:true
-    },
-    {
-      descricao:"Ir para a academia",
-      feito:false
-    },
-    {
-      descricao:"Estudar angular",
-      feito:true
-    },
-    {
-      descricao:"Dormir cedo",
-      feito:false
-    }
-  ];
+  allItems: Item[];
+
+  constructor(private storageService: StorageService){
+    this.allItems = storageService.getData(todoListStorageKey) || defaultToDoList;
+  }
+
+  saveList(){
+    this.storageService.setData(todoListStorageKey, this.allItems);
+  }
 
   addItem(descricao:string){
     this.allItems.push({
       descricao,
       feito:false
-    })
+    });
+    this.saveList();
   }
 
   removeItem(item:Item){
     this.allItems.splice(this.allItems.indexOf(item),1);
+    this.saveList();
   }
 }
